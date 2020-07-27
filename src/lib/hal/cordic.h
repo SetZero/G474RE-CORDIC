b54bd4743e55fe64a90b27c877069c7f7d230137
DevCodeOne <christian.r.dev@googlemail.com>
2020-07-27 11:15:15 +0200

    Added different header file for alle the operation and result types

--- src/lib/hal/cordic.h
+++ src/lib/hal/cordic.h
@@ -3,128 +3,13 @@
 #include "hal/cordic_types.h"
 #include "hal/hal_info.h"
 #include "hal/register.h"
+#include "hal/cordic/cordic_operations.h"
 
 template<typename MCU, typename PIN>
 concept cordic_mcu = stm_mcu<MCU, PIN>;
 
 namespace hal::cordic {
-    enum struct functions : uint8_t {
-        cosine,
-        sine,
-        phase,
-        modulus,
-        arctangent,
-        hyperbolic_cosine,
-        hyperbolic_sine,
-        arctanh,
-        natural_logarithm,
-        square_root
-    };
-
-    enum class operation_type { single, pipeline };
-
-    enum class cordic_algorithm_precision : uint8_t { normal = 8u };
-
-    template<precision P, cordic_algorithm_precision A = cordic_algorithm_precision::normal>
-    class cordic_config final {
-       public:
-        using qtype = Detail::precision_to_type<P>;
-
-        static inline constexpr auto precision = P;
-        static inline constexpr auto calculation_precision = A;
-    };
-
-    template<typename config, operation_type Type, functions Function>
-    class operation final {};
-
-    template<typename ResultType, operation_type Type, functions Function>
-    class operation_result final {};
-
-    // TODO: consider renaming functions to set args (angle and modulus in this case)
-    template<typename Config>
-    class operation<Config, operation_type::single, functions::cosine> final {
-       public:
-        using config_type = Config;
-        using thiz_type = operation<Config, operation_type::single, functions::cosine>;
-        using result_type = operation_result<typename config_type::qtype, operation_type::single, functions::cosine>;
-        using angle_type = angle<config_type::precision>;
-
-        thiz_type &arg1(angle_type angle) {
-            m_angle = angle;
-            return *this;
-        }
-
-        auto arg1() const { return static_cast<typename config_type::qtype>(m_angle); }
-
-        auto arg2() const { return m_modulus; }
-
-        auto scale() const { return 0u; }
-
-       private:
-        angle_type m_angle{0.0f};
-        typename config_type::qtype m_modulus{1.0f};
-    };
-
-    template<typename Config>
-    class operation<Config, operation_type::single, functions::sine> final {
-       public:
-        using config_type = Config;
-        using thiz_type = operation<Config, operation_type::single, functions::sine>;
-        using result_type = operation_result<typename config_type::qtype, operation_type::single, functions::sine>;
-        using angle_type = angle<config_type::precision>;
-
-        thiz_type &arg1(angle_type angle) {
-            m_angle = angle;
-            return *this;
-        }
-
-        auto arg1() const { return static_cast<typename config_type::qtype>(m_angle); }
-
-        auto arg2() const { return m_modulus; }
-
-        auto scale() const { return 0u; }
-
-       private:
-        angle_type m_angle{0.0f};
-        typename config_type::qtype m_modulus{1.0f};
-    };
-
-    // TODO: add multiple results
-    template<typename ResultType>
-    class operation_result<ResultType, operation_type::single, functions::cosine> final {
-       public:
-        using result_type = ResultType;
-        using thiz_type = operation_result<ResultType, operation_type::single, functions::cosine>;
-
-        thiz_type &result(ResultType result) {
-            m_result = result;
-            return *this;
-        }
-
-        result_type result() const { return m_result; }
-
-       private:
-        ResultType m_result;
-    };
-
-    template<typename ResultType>
-    class operation_result<ResultType, operation_type::single, functions::sine> final {
-       public:
-        using result_type = ResultType;
-        using thiz_type = operation_result<ResultType, operation_type::single, functions::sine>;
-
-        thiz_type &result(ResultType result) {
-            m_result = result;
-            return *this;
-        }
-
-        result_type result() const { return m_result; }
-
-       private:
-        ResultType m_result;
-    };
-
-    template<typename CordicNr, cordic_mcu<CordicNr> mcu>
+        template<typename CordicNr, cordic_mcu<CordicNr> mcu>
     class cordic {
        public:
         static inline constexpr auto cordic_register = hal::address<typename mcu::CORDIC, CordicNr>;
